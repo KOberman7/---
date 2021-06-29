28d306452da51994581b58337f67a9d3d7918c3a
Vera Koberman <50861303+KOberman7@users.noreply.github.com>
2021-06-29 21:08:23 +0500

    Add files via upload

---
+++ les_6/cart.js
@@ -0,0 +1,61 @@
+const catalog = {
+    products: [
+        {
+            id: 111,
+            name: 'Мышка',
+            price: 5000,
+        },
+
+        {
+            id: 222,
+            name: 'Монитор',
+            price: 11000,
+        },
+
+        {
+            id: 333,
+            name: 'Клавиатура',
+            price: 6000,
+        },
+
+        {
+            id: 444,
+            name: 'Видеокарта',
+            price: 62000,
+        },
+    ]
+}
+
+// var catalogArray = new Array;
+// for (var o in catalog) {
+//     catalogArray.push(catalog[o]);
+// }
+
+// //console.log(catalogArray);
+
+// const firstProduct = catalogArray[0][0];
+// const secondProduct = catalogArray[0][1];
+// const thirdProduct = catalogArray[0][2];
+// const fourthProduct = catalogArray[0][3];
+// //console.log(firstProduct);
+
+// function product(prop) {
+//     const id = prop.id;
+//     const name = prop.name;
+//     const price = prop.price;
+//     return name, price;
+//     //console.log(id);
+// }
+
+// function renderCatalog() {
+//     // const secondDiv = document.createElement('div');
+//     // secondDiv.className = 'catalog-products';
+//     // secondDiv.innerHTML = '';
+//     const productCard = document.createElement('p');
+//     productCard.className = 'product-card';
+//     productCard.innerHTML = product(firstProduct);
+// }
+
+// document.body.querySelector('.catalog').append(renderCatalog());
+
+
